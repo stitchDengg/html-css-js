f430ccfd1ce8b825abb77b082adb4f13f47caa56
Your Name <1051766345@qq.com>
2022-07-27 11:52:09 +0800

    commit

---
+++ es6/model模块和Babel编译/Babel和Webpack/Babel/使用babel编译es6代码/src/babel.js
@@ -0,0 +1,10 @@
+let name = 'alex';
+const age = 18;
+const add = (x,y) => x + y;
+new Promise((resolve,reject) => {
+    resolve('成功');
+})
+
+Array.from([1,2]);
+class person {}
+import 'index.js';
